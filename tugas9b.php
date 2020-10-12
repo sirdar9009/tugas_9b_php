a6cd813b303ce76d87e1ab56642a1ce458f11ebf
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-10-12 21:53:39 +0700

    Add files via upload

---
+++ tugas9b.php
@@ -0,0 +1,20 @@
+<?php
+for ($i=1; $i <= 200 ; $i++) {     // for 1, adalah bilangan yang akan di cek
+
+    $t = 0;
+
+        for ($j=1; $j <= $i ; $j++) {  // for 2, bilangan pembagi
+
+            if ($i % $j == 0) {
+                $t++;
+            }
+
+        }
+
+    if ($t == 2) {   // syarat atau kondisi bilangan prima
+        echo "".$i.", ";
+    }
+}
+
+
+ ?>
